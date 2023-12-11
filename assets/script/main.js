f3856b510a00cadd184eee0ac64c6892f9c2ab4c
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2023-12-11 11:46:30 +0100

    Feedback update

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,15 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+const card = document.querySelector("a:nth-of-type(15)");
+
+card.addEventListener("dblclick", (e) => {
+  card.classList.toggle("large");
+});
+
+// const card = document.querySelector("a:nth-of-type(2)");
+
+// card.addEventListener("click", (e) => {
+//   card.classList.toggle("large");
+// });
